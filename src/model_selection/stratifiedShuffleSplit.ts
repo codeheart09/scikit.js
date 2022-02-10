61e07eb4a9c1e6966adbacb3a672a00a04e9e90f
codeheart09 <codeheart09@gmail.com>
2022-02-10 13:30:03 -0300

    WIP: StratifiedShuffleSplit - starting validation

--- src/model_selection/stratifiedShuffleSplit.ts
+++ src/model_selection/stratifiedShuffleSplit.ts
@@ -1,25 +1,53 @@
+/**
+ *  @license
+ * Copyright 2022, JsData. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==========================================================================
+ */
+
 import { CrossValidator } from './crossValidator';
 import { Scikit1D, Scikit2D } from '../types';
+import { assert } from '../typesUtils'
 
 export interface StratifiedShuffleSplitParams {
-  nSplits: number;
-  trainSize?: number;
+  nSplits?: number;
   testSize?: number;
+  trainSize?: number;
   randomState?: number;
 }
 
 export class StratifiedShuffleSplit implements CrossValidator {
-  nSplits
-  trainSize
-  testSize
-  randomState
+  nSplits: number
+  testSize?: number
+  trainSize?: number
+  randomState?: number
 
   constructor({
-    nSplits,
-    trainSize,
+    nSplits = 10,
     testSize,
+    trainSize,
     randomState
-  }: StratifiedShuffleSplitParams = { nSplits: 10 }) {
+  }: StratifiedShuffleSplitParams = {}) {
+    // Parse & validate: nSplits
+    nSplits = Number(nSplits)
+    assert(
+      Number.isInteger(nSplits) && nSplits > 1,
+      'new StratifiedShuffleSplit({nSplits}): nSplits must be an int greater than 1.'
+    )
+
+    // Parse & validate: testSize
+    if (testSize === undefined && trainSize === undefined) {
+      testSize = 0.1
+    }
+
     // @todo validate before storing
     this.nSplits = nSplits
     this.trainSize = trainSize
